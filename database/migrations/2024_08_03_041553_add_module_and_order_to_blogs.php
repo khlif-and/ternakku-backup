140b0f3c61142bdfb14da0f96cc73f79e656ce02
muhammad iqbal Mubarok <mubarok.iqbal@gmail.com>
2024-08-03 11:51:55 +0700

    blogs

---
+++ database/migrations/2024_08_03_041553_add_module_and_order_to_blogs.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('blogs', function (Blueprint $table) {
+            $table->unsignedInteger('order')->deafult(1);
+            $table->string('module')->default('qurban');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('blogs', function (Blueprint $table) {
+            $table->dropColumn(['module' , 'order']);
+        });
+    }
+};
