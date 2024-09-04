f66aa4322f3bfccc0e6fa7264b2b4db4d5949c51
muhammad iqbal Mubarok <mubarok.iqbal@gmail.com>
2024-09-04 23:35:48 +0700

    milk analysis global

--- app/Http/Controllers/Api/Farming/MilkAnalysisGlobalController.php
+++ app/Http/Controllers/Api/Farming/MilkAnalysisGlobalController.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Farming\MilkAnalysisGlobalResource;
 use App\Http\Requests\Farming\MilkAnalysisGlobalStoreRequest;
+use App\Http\Requests\Farming\MilkAnalysisGlobalUpdateRequest;
 
 class MilkAnalysisGlobalController extends Controller
 {
@@ -63,6 +64,89 @@ class MilkAnalysisGlobalController extends Controller
 
             // Handle exceptions and return an error response
             return ResponseHelper::error(null, 'An error occurred while recording the data.', 500);
+        }
+    }
+
+    public function show(int $farmId, int $id): JsonResponse
+    {
+        try {
+            // Get the farm from request attributes
+            $farm = request()->attributes->get('farm');
+
+            $livestockDeath = MilkAnalysisGlobal::where('farm_id', $farm->id)->findOrFail($id);
+
+            return ResponseHelper::success(new MilkAnalysisGlobalResource($livestockDeath), 'Data retrieved successfully.');
+
+        } catch (\Exception $e) {
+            // Handle exceptions and return an error response
+            return ResponseHelper::error(null, 'An error occurred while retrieving the data.', 500);
+        }
+    }
+
+    public function update(MilkAnalysisGlobalUpdateRequest $request, $farmId, $id): JsonResponse
+    {
+        DB::beginTransaction();
+
+        try {
+            // Get the farm from request attributes
+            $farm = request()->attributes->get('farm');
+
+            // Retrieve the validated input data
+            $validated = $request->validated();
+
+            $data = MilkAnalysisGlobal::where('farm_id', $farm->id)->findOrFail($id);
+
+            $data->update([
+                'transaction_date' => $validated['transaction_date'],
+                'bj' => $validated['bj'] ?? null,
+                'at' => $validated['at'] ?? null,
+                'ab' => $validated['ab'] ?? null,
+                'mbrt' => $validated['mbrt'] ?? null,
+                'a_water' => $validated['a_water'] ?? null,
+                'protein' => $validated['protein'] ?? null,
+                'fat' => $validated['fat'] ?? null,
+                'snf' => $validated['snf'] ?? null,
+                'ts' => $validated['ts'] ?? null,
+                'rzn' => $validated['rzn'] ?? null,
+                'notes' => $validated['notes'] ?? null
+            ]);
+
+            DB::commit();
+
+            // Return the updated resource using LivestockDeathResource
+            return ResponseHelper::success(new MilkAnalysisGlobalResource($data), 'Data updated successfully.');
+
+        } catch (\Exception $e) {
+            DB::rollBack();
+
+            // Handle exceptions and return an error response
+            return ResponseHelper::error(null, 'An error occurred while updating the data.', 500);
+        }
+    }
+
+
+    public function destroy(int $farmId, int $id): JsonResponse
+    {
+        DB::beginTransaction();
+
+        try {
+            // Get the farm from request attributes
+            $farm = request()->attributes->get('farm');
+
+            $data = MilkAnalysisGlobal::where('farm_id', $farm->id)->findOrFail($id);
+
+            $data->delete();
+
+            DB::commit();
+
+            // Return a success response
+            return ResponseHelper::success(null, 'Data deleted successfully.');
+
+        } catch (\Exception $e) {
+            DB::rollBack();
+
+            // Handle exceptions and return an error response
+            return ResponseHelper::error(null, 'An error occurred while deleting the data.', 500);
         }
     }
 }
