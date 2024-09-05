d06f6f9e455bcdc7eaf9930d3370174d7fb65518
muhammad iqbal Mubarok <mubarok.iqbal@gmail.com>
2024-09-05 20:48:33 +0700

    milk production global

---
+++ database/migrations/2024_09_05_114542_create_feed_medicine_purchase_d_s_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('feed_medicine_purchase_d', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('feed_medicine_purchase_h_id');
+            $table->enum('purchase_type', ['feed', 'medicine']);
+            $table->string('item_name');
+            $table->decimal('quantity', 8, 2);
+            $table->string('unit');
+            $table->decimal('price_per_unit', 10, 2);
+            $table->decimal('total_price', 10, 2);
+            $table->timestamps();
+
+            $table->foreign('feed_medicine_purchase_h_id')->references('id')->on('feed_medicine_purchase_h')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('feed_medicine_purchase_d');
+    }
+};
