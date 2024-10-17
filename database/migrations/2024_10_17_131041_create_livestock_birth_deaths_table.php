83f9e38d5356ca4347d9e76ff7606d7b2d7c2255
muhammad iqbal Mubarok <mubarok.iqbal@gmail.com>
2024-10-17 20:13:01 +0700

    ternak melahirkan

---
+++ database/migrations/2024_10_17_131041_create_livestock_birth_deaths_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('livestock_birth_deaths', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('livestock_birth_id');
+
+            $table->timestamps();
+            $table->foreign('livestock_birth_id')->references('id')->on('livestock_birth')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('livestock_birth_deaths');
+    }
+};
