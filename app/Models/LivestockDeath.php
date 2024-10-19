4bfac7189e40f44d90ed4645dec2e6a386fffe63
muhammad iqbal Mubarok <mubarok.iqbal@gmail.com>
2024-10-19 20:04:45 +0700

    created_at

--- app/Models/LivestockDeath.php
+++ app/Models/LivestockDeath.php
@@ -41,7 +41,7 @@ class LivestockDeath extends Model
         $lastTransaction = self::whereYear('transaction_date', $date->year)
             ->whereMonth('transaction_date', $date->month)
             ->where('farm_id' , $farmId)
-            ->orderBy('transaction_number', 'desc')
+            ->orderBy('created_at', 'desc')
             ->first();
 
         if ($lastTransaction) {
--- app/Models/MilkProductionGlobal.php
+++ app/Models/MilkProductionGlobal.php
@@ -36,7 +36,7 @@ class MilkProductionGlobal extends Model
         $lastTransaction = self::whereYear('transaction_date', $date->year)
             ->whereMonth('transaction_date', $date->month)
             ->where('farm_id' , $farmId)
-            ->orderBy('transaction_number', 'desc')
+            ->orderBy('created_at', 'desc')
             ->first();
 
         if ($lastTransaction) {
