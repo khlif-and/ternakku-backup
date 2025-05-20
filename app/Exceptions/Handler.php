2ce6627ae8bb895b44e0f3e5cc6817859dad0411
Muhammad Iqbal Mubarok <muhammadiqbalmubarok@Muhammads-MacBook-Pro.local>
2025-05-20 18:31:37 +0700

    add sentry

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,11 +4,12 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Sentry\State\HubInterface;
 
 class Handler extends ExceptionHandler
 {
     /**
-     * The list of the inputs that are never flashed to the session on validation exceptions.
+     * A list of the inputs that are never flashed for validation exceptions.
      *
      * @var array<int, string>
      */
@@ -19,12 +20,25 @@ class Handler extends ExceptionHandler
     ];
 
     /**
+     * Report or log an exception.
+     */
+    public function report(Throwable $exception): void
+    {
+        // Kirim ke Sentry jika exception ini seharusnya dilaporkan
+        if ($this->shouldReport($exception) && app()->bound('sentry')) {
+            \Sentry\captureException($exception);
+        }
+
+        parent::report($exception);
+    }
+
+    /**
      * Register the exception handling callbacks for the application.
      */
     public function register(): void
     {
         $this->reportable(function (Throwable $e) {
-            //
+            // Kamu bisa log custom error di sini jika mau
         });
     }
 }
