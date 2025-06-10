dd4819b5629b1b7d0ce50d225095c1ed3a13feba
mubarok-iqbal <mubarok.iqbal@gmail.com>
2025-06-10 19:26:42 +0700

    sub

--- database/migrations/2025_06_10_114604_populate_subs_table.php
+++ database/migrations/2025_06_10_114604_populate_subs_table.php
@@ -13,13 +13,17 @@ return new class extends Migration
      */
     public function up(): void
     {
-        DB::table('subscriptions')->firstOrCreate(
-            ['id' => 1], // Attributes to search by
-            [
+        // Check if the record already exists
+        $subscriptionExists = DB::table('subscriptions')->where('id', 1)->exists();
+
+        if (!$subscriptionExists) {
+            DB::table('subscriptions')->insert([
+                'id' => 1,
                 'module' => 'farming',
                 'name' => 'basic',
-            ] // Attributes to create if not found
-        );
+            ]);
+        }
+
         $farms = DB::table('farms')->get();
 
         // Loop melalui setiap farm dan masukkan ke tabel 'subscription_farm'
